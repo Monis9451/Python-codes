99cb477ea5579f059ae72370e68aef21c681e1ea
Monis9451 <mianmonishussain786@gmail.com>
2024-06-09 16:06:28 +0500

    new file for greetings according to time

--- Greets according to time.py
+++ Greets according to time.py
@@ -1,12 +1,8 @@
-# import time
-# times = time.strftime('%H')
-# if((int(times))>4 and (int(times))<=12):
-#     print("Good Morning!")
-# elif((int(times))>12 and (int(times))<=18):
-#     print("Good After Noon!")
-# elif(((int(times))>18 and (int(times))<=24)or(int(times))<=3):
-#     print("Good Night!")
-
-a = "monis"
-b= "10"
-print(int(a)*b)
+import time
+times = time.strftime('%H')
+if((int(times))>4 and (int(times))<=12):
+    print("Good Morning!")
+elif((int(times))>12 and (int(times))<=18):
+    print("Good After Noon!")
+elif(((int(times))>18 and (int(times))<=24)or(int(times))<=3):
+    print("Good Night!")
