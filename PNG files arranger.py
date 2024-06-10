b4f1e164e3abc2c2b6c7367207968257035bf624
Monis9451 <mianmonishussain786@gmail.com>
2024-06-10 16:46:47 +0500

    Files arranger in python

---
+++ PNG files arranger.py
@@ -0,0 +1,14 @@
+import os
+extention = input('Enter a valid extention name like .png, .pdf : ')
+path = input('Enter the path of the files: ')
+# extention = '.png'
+#path = 'D:\\New folder'
+files = os.listdir(path)
+i=1
+for file in files:
+    if file.endswith(extention):
+        os.rename(f'D:\\New folder\\{file}',f'D:\\New folder\\{i}{extention}')
+        print(f'D:\\New folder\\{file} renamed')
+        i+=1
+        
+#This code will rename your files from 1 to n where n is the number of files in your folder or the type of entered extension.
