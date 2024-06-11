30dc3726f33ebc93295eb221aeb3d85990fb4f12
Monis9451 <mianmonishussain786@gmail.com>
2024-06-11 19:41:10 +0500

    making function

--- Greets according to time.py
+++ Greets according to time.py
@@ -1,8 +1,11 @@
 import time
-times = time.strftime('%H')
-if((int(times))>4 and (int(times))<=12):
-    print("Good Morning!")
-elif((int(times))>12 and (int(times))<=18):
-    print("Good After Noon!")
-elif(((int(times))>18 and (int(times))<=24)or(int(times))<=3):
-    print("Good Night!")
+def greeting():
+    times = time.strftime('%H')
+    if((int(times))>4 and (int(times))<=12):
+        print("Good Morning!")
+    elif((int(times))>12 and (int(times))<=18):
+        print("Good After Noon!")
+    elif(((int(times))>18 and (int(times))<=24)or(int(times))<=3):
+        print("Good Night!")
+        
+greeting()
